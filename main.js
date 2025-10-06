61701aa3a6b50fea7a8164ed2621d43b659abded
Yusuf Ahmed <yusufahmed0625@gmail.com>
2025-10-06 15:03:31 +0100

    created logic for total amount

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const invoiceTable = document.getElementById('invoice-table');
 
+
+
 const addNewTableRow = () => {
     invoiceTable.insertAdjacentHTML('beforeend',`
         <tr>
@@ -10,3 +12,38 @@ const addNewTableRow = () => {
     `);
     
 }
+
+const pleasePayAmount = document.getElementById('please-pay-amount');
+
+let totalSum = 0;
+
+const tallyTotalSum = () => {
+    const priceInputs = document.querySelectorAll('.price-input');
+    totalSum = 0;
+
+    priceInputs.forEach(input => {
+        const value = parseFloat(input.value) || 0;
+        totalSum += value
+    })
+
+    console.log(totalSum)
+    return totalSum;
+}
+
+invoiceTable.addEventListener('input', e => {
+    if (e.target.classList.contains('price-input')){
+        tallyTotalSum();
+        pleasePayAmount.innerText = `Please pay £${totalSum.toFixed(2)} into the following bank account:`
+    }
+    
+})
+
+const titleFullNameInput = document.getElementById('title-full-name-input');
+const manyThanks = document.getElementById('many-thanks')
+
+titleFullNameInput.addEventListener('input', () => {
+    manyThanks.innerText = `Many Thanks, ${titleFullNameInput.value}`
+})
+
+
+
