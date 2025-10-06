55b7623a145271ddcdc5fcd443b23638c6f107f9
Yusuf Ahmed <yusufahmed0625@gmail.com>
2025-10-06 14:43:13 +0100

    added logic for adding new row to table and keeping previous row data

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log(1)
+const invoiceTable = document.getElementById('invoice-table');
+
+const addNewTableRow = () => {
+    invoiceTable.insertAdjacentHTML('beforeend',`
+        <tr>
+            <td><input class="date-input" type="date"></td>
+            <td><input class="desc-input" type="text"></td>
+            <td><input class="price-input" type="text"></td>
+        </tr>
+    `);
+    
+}
